76b4b81077c20005ff82c610aff90497eef48999
AgisTheSecond <132357245+AgisTheSecond@users.noreply.github.com>
2025-11-16 14:36:23 +0200

    assign3 add

---
+++ Assignment03_2025/src_corpus/audit_monitor.c
@@ -0,0 +1,120 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct log_entry {
+
+	int uid; /* user id (positive integer) */
+	pid_t pid; /* process id (positive integer) */
+
+	char *file; /* filename (string) */
+
+	time_t date; /* file access date - utc*/
+	time_t time; /* file access time - utc*/
+
+	int operation; /* access type values [0-3] */
+	int action_denied; /* is action denied values [0-1] */
+
+	char *filehash; /* file hash - sha256 - evp */
+
+	/* add here other fields if necessary */
+	/* ... */
+	/* ... */
+
+};
+
+
+void
+usage(void)
+{
+	printf(
+	       "\n"
+	       "usage:\n"
+	       "\t./audit_monitor \n"
+		   "Options:\n"
+		   "-s, Prints malicious users\n"
+		   "-i <filename>, Prints table of users that modified "
+		   "the file <filename> and the number of modifications\n"
+		   "-h, Help message\n\n"
+		   );
+
+	exit(1);
+}
+
+
+void 
+list_unauthorized_accesses(FILE *log)
+{
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+	return;
+
+}
+
+
+void
+list_file_modifications(FILE *log, char *file_to_scan)
+{
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+	return;
+
+}
+
+
+int 
+main(int argc, char *argv[])
+{
+
+	int ch;
+	FILE *log;
+
+	if (argc < 2)
+		usage();
+
+	log = fopen("./access_audit.log", "r");
+	if (log == NULL) {
+		printf("Error opening log file \"%s\"\n", "./access_audit.log");
+		return 1;
+	}
+
+	while ((ch = getopt(argc, argv, "hi:s")) != -1) {
+		switch (ch) {		
+		case 'i':
+			list_file_modifications(log, optarg);
+			break;
+		case 's':
+			list_unauthorized_accesses(log);
+			break;
+		default:
+			usage();
+		}
+
+	}
+
+
+	/* add your code here */
+	/* ... */
+	/* ... */
+	/* ... */
+	/* ... */
+
+
+	fclose(log);
+	argc -= optind;
+	argv += optind;	
+	
+	return 0;
+}
